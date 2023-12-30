1d6b7ef8193c2335d21b6190c91d1ab47b0f2141
TJC1996 <101535962+TJC1996@users.noreply.github.com>
2023-12-30 15:16:03 -0800

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,8 @@ export default function Main() {
         <main>
 
             <div className="v0_196">
-                <span className="v0_197">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</span>
-                <span className="v0_198">Food expert. Skateboarder. Reader. Internet fanatic. Nature Explorer. Entrepreneur. Travel geek. Beach Goer. Coffee fanatic.</span>
+                <span className="v0_197">I am a front-end developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</span>
+                <span className="v0_198">Food enjoyer. Skateboarder. Reader. Internet fanatic. Nature Explorer. Entrepreneur. Traveler. Beach Goer. Coffee fanatic.</span>
                 <span className="v0_199">About</span>
                 <span className="v0_200">Interests</span>
             </div>
